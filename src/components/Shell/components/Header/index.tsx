85671a4373d5ae388a668184c477d7fd8d0e815d
Liuver C. Espinosa <liuver.carrera.espinosa@gmail.com>
2022-08-12 16:22:10 -0400

    End second nav

--- src/components/Shell/components/Header/index.tsx
+++ src/components/Shell/components/Header/index.tsx
@@ -4,41 +4,89 @@ import SearchInput from '../../../SearchInput';
 
 import AccountIcon from '../../../../assets/images/account.png'; 
 import ShoppingBagIcon from '../../../../assets/images/shopping-bag.png'; 
+import classNames from 'classnames';
 
-const navigation = [
-  { name: 'Solutions', href: '#' },
-  { name: 'Pricing', href: '#' },
-  { name: 'Docs', href: '#' },
-  { name: 'Company', href: '#' },
+interface NavigationProps {
+  id: number;
+  href: string;
+  title: string;
+}
+const navigation: NavigationProps[] = [
+  {
+    id: 1,
+    href: '#',
+    title: 'All products',
+  },
+  {
+    id: 2,
+    href: '#',
+    title: 'Packaging',
+  },
+  {
+    id: 3,
+    href: '#',
+    title: 'Drinkware',
+  },
+  {
+    id: 4,
+    href: '#',
+    title: 'Apparel',
+  },
+  {
+    id: 5,
+    href: '#',
+    title: 'Backpacks',
+  },
+  {
+    id: 6,
+    href: '#',
+    title: 'Notebooks',
+  },
 ];
 
 function Header(): ReactElement {
+
   return (
-    <header className="bg-dark-01"> 
-      <nav className="max-w-[90rem] mx-auto px-3 py-5" aria-label="Top">
-        <div className="w-full flex items-center justify-between">
-          <div className="flex items-center w-full max-w-3xl">
-            <Logo />
-            <SearchInput className="ml-[63px] w-full" placeholder="Search products" />            
+    <header>
+      <div className="bg-dark-01">
+        <nav className="max-w-[90rem] mx-auto px-3 py-5" aria-label="Top">
+          <div className="w-full flex items-center justify-between">
+            <div className="flex items-center w-full max-w-3xl">
+              <Logo />
+              <SearchInput className="ml-[63px] w-full" placeholder="Search products" />            
+            </div>
+            <div className="ml-10 space-x-4 inline-flex items-center">
+              <a
+                href="#"
+                className="inline-flex items-center text-[15px] leading-[16px] font-normal text-white"
+              >
+                <img src={AccountIcon} alt="Account" />
+                <span className="ml-3 whitespace-nowrap">Sign in</span>
+              </a>
+              <a
+                href="#"
+                className="inline-flex items-center text-[15px] leading-[16px] font-normal text-white"
+              >
+                <img src={ShoppingBagIcon} alt="Cart" />
+                <span className="ml-3">Cart</span>
+              </a>
+            </div>
           </div>
-          <div className="ml-10 space-x-4 inline-flex items-center">
-            <a
-              href="#"
-              className="inline-flex items-center text-[15px] leading-[16px] font-normal text-white"
-            >
-              <img src={AccountIcon} alt="Account" />
-              <span className="ml-3 whitespace-nowrap">Sign in</span>
-            </a>
-            <a
-              href="#"
-              className="inline-flex items-center text-[15px] leading-[16px] font-normal text-white"
-            >
-              <img src={ShoppingBagIcon} alt="Cart" />
-              <span className="ml-3">Cart</span>
-            </a>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
+      <div className="bg-dark-10">
+        <nav className="max-w-[90rem] mx-auto px-3 py-5" aria-label="Below">
+          <ul className='inline-flex gap-x-6 text-[15px] leading-[16px] font-normal text-dark-04'>
+            {navigation.map( ( item: NavigationProps ) => ( 
+              <li key={item.id}>
+                <a href={item.href}>
+                  {item.title}
+                </a>
+              </li>
+            ) )}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
